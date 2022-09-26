58c54d24ed5372e355f2a0974845b06a9781241a
bozo.kopic <bozo@kopic.xyz>
2022-09-26 23:49:39 +0200

    minor build fix

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 cd $(dirname -- "$0")
 
 LIBS="gtk+-3.0 gio-2.0 gio-unix-2.0 glib-2.0"
-CC=${CC:-gcc}
+CC=${CC:-cc}
 
 mkdir -p build
 $CC -o build/mbgui src_c/*.c $(pkg-config --cflags --libs $LIBS)
